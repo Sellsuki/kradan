3bfe57f8cdb512610264bcf193433868680327a1
Nati Namvong <nati@flyingcomma.com>
2016-10-15 02:03:33 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="">
+
+  </div>
+</template>
+
+<script>
+// import hljs from 'highlight.js'
+// var data = {}
+
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  mounted () {},
+  methods: {
+  },
+  components: {
+  }
+}
+</script>
+<style lang="css">
+</style>
